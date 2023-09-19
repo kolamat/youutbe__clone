7f2fa29d5225a1b3b8552054476ce435ae115a74
kolamat <132449653+kolamat@users.noreply.github.com>
2023-09-19 02:45:46 -0700

    Changes to be committed: 	modified:   public/index.html 	modified:   src/App.js 	new file:   src/components/ChannelDetail.jsx 	new file:   src/components/Feed.jsx 	new file:   src/components/Navbar.jsx 	new file:   src/components/SearchFeed.jsx 	new file:   src/components/VideoDetail.jsx 	new file:   src/components/index.js 	renamed:    src/styles.css -> src/index.css 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./components";
 const app = () => {
-  return <div>App</div>;
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: "#000" }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/Channel/:id" element={<ChannelDetail />} />
+        <Route path="/Search/:SearchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>;
 };
 
 export default app;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { ReactDOM } from "react";
 
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createroot(document.getElementById("root"));
 
 root.render(<App />);
